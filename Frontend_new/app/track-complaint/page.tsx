fddd5a0828d29dee7ca5cef2c04b1daf84ee42d6
Kartik <143881752+Ch-Kumar-Kartik@users.noreply.github.com>
2025-02-22 01:43:48 +0530

    Update page.tsx

--- Frontend_new/app/track-complaint/page.tsx
+++ Frontend_new/app/track-complaint/page.tsx
@@ -123,16 +123,16 @@ const TrackComplaint: React.FC = () => {
                     <td className="px-6 py-4 whitespace-nowrap">Verified</td>
                   </tr>
                   <tr>
-                    <td className="px-6 py-4 whitespace-nowrap">Department Allocation</td>
-                    <td className="px-6 py-4 whitespace-nowrap">Asssigned</td>
+                    <td className="px-6 py-4 whitespace-nowrap">Department</td>
+                    <td className="px-6 py-4 whitespace-nowrap">Verified</td>
                   </tr>
                   <tr>
-                    <td className="px-6 py-4 whitespace-nowrap">Handling Authority</td>
-                    <td className="px-6 py-4 whitespace-nowrap">Assigned</td>
+                    <td className="px-6 py-4 whitespace-nowrap">Manpower Department</td>
+                    <td className="px-6 py-4 whitespace-nowrap">Yes</td>
                   </tr>
                   <tr>
                     <td className="px-6 py-4 whitespace-nowrap">Final Status</td>
-                    <td className="px-6 py-4 whitespace-nowrap">Closed/td>
+                    <td className="px-6 py-4 whitespace-nowrap">Resolved</td>
                   </tr>
                 </tbody>
               </table>
@@ -152,4 +152,3 @@ const TrackComplaint: React.FC = () => {
 }
 
 export default TrackComplaint
-
